67cd3886528a2db86f72f86ec5ecbed8e66c488c
papercut <papercut@172.17.61.48>
2019-08-13 16:27:56 +0700

    2commit

--- src/chap3/CallEmployee.java
+++ src/chap3/CallEmployee.java
@@ -1,14 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package chap3;
 
-/**
- *
- * @author papercut
- */
 public class CallEmployee {
+    public static void main (String[] arge) {
+        //call static attr & method
+        System.out.println("Id : "+Employee.id);
+        Employee.work();
     
+        //call non-static
+        Employee em = new Employee();
+        System.out.println("Name : "+em.name);
+        System.out.println("Age : "+em.age);
+        em.getSalary();
+    }   
 }
--- src/chap3/Employee.java
+++ src/chap3/Employee.java
@@ -1,14 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package chap3;
 
-/**
- *
- * @author papercut
- */
 public class Employee {
+    static int id = 1001;
+    String name = "Adssadayut";
+    int age = 35;
     
+    static void work(){
+        System.out.println("work()");
+    }
+    
+    void getSalary(){
+        System.out.println("getSalary()");
+    }
+    public static void main (String[] args) {
+        //call static attr & method
+        System.out.println("Id : "+id);
+        work();
+        
+        //call non-static
+        Employee em = new Employee();
+        System.out.println("Name : "+em.name);
+        System.out.println("Age : "+em.age);
+        em.getSalary();
+        
+    }
 }
